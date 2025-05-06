4da448b496e9815db900ec7e59321a3f6419dc75
o_8uzi <99588257+djsiskovits@users.noreply.github.com>
2025-05-07 10:00:39 +1200

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -8,17 +8,12 @@ local dba=false local dbc=false local ra=game.ReplicatedStorage.Network:InvokeSe
 local rs=game:GetService("RunService") local rss=rs.Stepped
 local ia = vd.PlayerGui.ScreenGui.StatsFrame2.Inventory.Amount
 
-local ua = Instance.new("ScreenGui",vd)
-local ub = Instance.new("Frame",ua)
-local uc = Instance.new("TextLabel",ub)
-local ud = Instance.new("Frame",ub)
-local uf = Instance.new("UITableLayout",ud)
-local ue = Instance.new("TextButton",ud)
-local ug = Instance.new("TextButton",ud)
-local uh = Instance.new("TextButton",ud)
-local ui = Instance.new("TextButton",ud)
-local uj = Instance.new("TextButton",ud)
-local uk = Instance.new("TextButton",ud)
+local ua=Instance.new("ScreenGui",vd);local ub=Instance.new("Frame",ua)
+local uc=Instance.new("TextLabel",ub);local ud=Instance.new("Frame",ub)
+local uf=Instance.new("UITableLayout",ud);local ue=Instance.new("TextButton",ud)
+local ug=Instance.new("TextButton",ud);local uh=Instance.new("TextButton",ud)
+local ui=Instance.new("TextButton",ud);local uj=Instance.new("TextButton",ud)
+local uk=Instance.new("TextButton",ud)
 
 for i,oldgui in (vd.PlayerGui:GetDescendants()) do
 if oldgui:FindFirstChild("o_8uzi#7674crediting") then oldgui:Destroy() end
@@ -124,42 +119,4 @@ ui.MouseButton1Click:Connect(function()
     end
 end)
 
-local function GRIDO_fake_script() -- mainframe.DraggableGUI 
-	local script = Instance.new('LocalScript', ub)
-	local UserInputService = game:GetService("UserInputService")
-	local gui = script.Parent
-	local dragging
-	local dragInput
-	local dragStart
-	local startPos
-	local function update(input)
-		local delta = input.Position - dragStart
-		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	end
-	gui.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-			dragging = true
-			dragStart = input.Position
-			startPos = gui.Position
-			
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragging = false
-				end
-			end)
-		end
-	end)
-	gui.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end)
-	UserInputService.InputChanged:Connect(function(input)
-		if input == dragInput and dragging then
-			if gui.Visible then
-				update(input)
-			end
-		end
-	end)
-end
 coroutine.wrap(GRIDO_fake_script)()
